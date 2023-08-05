32b15df7baa39009a2db65cab241c43ed0cdf763
Said Gandur <ing.saidgandur@gmail.com>
2023-08-04 23:00:35 -0500

    test page index

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        {
+          charset: 'utf-8'
+        }
+      ],
+      title: 'Draft project',
+      link: [],
+      style: [],
+      script: [],
+      noscript: []
+    }
+  },
+  css: ['@/assets/css/base.css'],
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/eslint-module'
+  ],
+  googleFonts: {
+    families: {
+      Poppins: {
+        wght: [100,200,300,400,500,600,700,800]
+      },
+      Archivo: {
+        wght: [100, 400, 500, 600, 700, 800]
+      }
+    }
+  },
   devtools: { enabled: true }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
+    This is code from the page
     <NuxtWelcome />
-    test 2
   </div>
 </template>
