9eb2921aa77400da7f9f4181fea0a18f0e1f3552
Said Gandur <ing.saidgandur@gmail.com>
2023-08-15 21:39:21 -0500

    New black berry page has been created

---
+++ pages/adidas.vue
@@ -0,0 +1,381 @@
+<template>
+  <div class="container">
+    <div class="overlay first"></div>
+    <div class="overlay second"></div>
+    <div class="overlay third"></div>
+
+    <div class="nav">
+      <div class="logo">adidas<span class="reg">&reg;</span></div>
+      <div class="menu"><font-awesome-icon :icon="['fas', 'bars']" /></div>
+      <div class="search"><font-awesome-icon :icon="['fas', 'magnifying-glass']" /></div>
+      <div class="bag"><font-awesome-icon :icon="['fas', 'bag-shopping']" /></div>
+    </div>
+
+    <img :src="imgHero" alt="" class="product-img" />
+
+    <div class="product-text">
+      <h1 class="product-title">adidas.</h1>
+    </div>
+
+    <div class="product-desc">
+      <p>The pure comfort is here !</p>
+      <button>reserve now</button>
+    </div>
+
+    <div class="size">
+      <ul>
+        <span>Size :</span>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+    </div>
+
+    <div class="bottom-img"></div>
+
+    <div class="bottom-right left-button">
+      <ul>
+        <li><font-awesome-icon :icon="['fas', 'chevron-left']" /></li>
+        <li><font-awesome-icon :icon="['fas', 'chevron-right']" /></li>
+      </ul>
+    </div>
+
+    <div class="media">
+      <ul>
+        <li><font-awesome-icon class="icon" :icon="['fab', 'facebook-f']" /></li>
+        <li><font-awesome-icon class="icon" :icon="['fab', 'instagram']" /></li>
+        <li><font-awesome-icon class="icon" :icon="['fab', 'twitter']" /></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="js" setup>
+import imgHero from '~/assets/img/adidas/hero.png'
+onMounted(() => {
+  TweenMax.to('.first', 1.5, {
+            delay: .2,
+            left: '-100%',
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.to('.second', 1.5, {
+            delay: .4,
+            left: '-100%',
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.to('.third', 1.5, {
+            delay: .6,
+            left: '-100%',
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from('.logo', 1, {
+            delay: 1,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.menu', 1, {
+            delay: 1.2,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from('.search', .8, {
+            delay: 1.6,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from('.bag', 1, {
+            delay: 1.6,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.staggerFrom('.media ul li', 1, {
+            delay: 2,
+            opacity: 0,
+            x: -20,
+            ease: Power3.easeInOut
+        }, 0.08)
+
+        TweenMax.from('.size', 1, {
+            delay: 1.8,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+        TweenMax.staggerFrom('.size ul li', .3, {
+            delay: 2,
+            opacity: 0,
+            y: 20,
+            ease: Power3.easeInOut
+        }, 0.08)
+        TweenMax.from('.bottom-right ul li:first-child', .5, {
+            delay: 2.4,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.bottom-right ul li:last-child', .6, {
+            delay: 2.4,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.bottom-img', 1, {
+            delay: 2.4,
+            opacity: 0,
+            x: -20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from('.product-title', 3, {
+            delay: 2.2,
+            opacity: 0,
+            y: 50,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.product-img', 1, {
+            delay: 4.2,
+            opacity: 0,
+            y: -800,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.product-desc p', 3, {
+            delay: 4.5,
+            opacity: 0,
+            y: -50,
+            ease: Expo.easeInOut
+        })
+        TweenMax.from('.product-desc button', 3, {
+            delay: 6,
+            opacity: 0,
+            y: -50,
+            ease: Expo.easeInOut
+        })
+})
+</script>
+<style lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,800;0,900;1,900&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100vh;
+  font-family: 'poppins';
+  font-size: 14px;
+  background-color: #8c6c79;
+  color: #fff;
+  overflow: hidden;
+}
+
+.nav {
+  position: fixed;
+  width: 100%;
+  height: 100px;
+  top: 10px;
+}
+
+.logo,
+.menu,
+.search,
+.bag {
+  position: absolute;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+.logo {
+  left: 20px;
+  font-size: 20px;
+  font-weight: bolder;
+}
+
+.menu {
+  left: 150px;
+  top: 4px;
+  line-height: 1.2;
+  font-size: 20px;
+}
+
+.search,
+.bag {
+  background: #fff;
+  color: black;
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  text-align: center;
+  margin-top: 18px;
+}
+
+.search {
+  right: 140px;
+}
+
+.bag {
+  right: 40px;
+}
+
+.product-img {
+  width: 900px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -40%);
+  z-index: 2;
+}
+
+.product-text {
+  position: absolute;
+  top: 43%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.product-title {
+  color: #f2d16b;
+  font-weight: 900;
+  letter-spacing: -10px;
+  font-size: 280px;
+}
+
+.product-desc {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  bottom: -585px;
+  z-index: 2;
+}
+
+.product-desc p {
+  position: absolute;
+  font-size: 20px;
+  top: -500px;
+  letter-spacing: 20px;
+  text-transform: uppercase;
+}
+
+.product-desc button {
+  margin-top: 10px;
+  background: #f2d16b;
+  color: black;
+  font-weight: bold;
+  padding: 15px 30px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 10px;
+  border: none;
+  border-radius: 30px;
+}
+
+.size {
+  position: absolute;
+  left: 50px;
+  bottom: 40px;
+}
+
+.size ul {
+  list-style: none;
+}
+
+.size ul li {
+  display: inline-block;
+  font-weight: 500;
+  border: 1px solid #fff;
+  padding: 5px;
+  border-radius: 50%;
+  margin-left: 10px;
+  height: 20px;
+  width: 20px;
+  text-align: center;
+  line-height: 1.5;
+  transition: 0.3s ease-in-out;
+}
+
+.size span {
+  font-weight: 500;
+}
+
+.size span::after {
+  display: inline-block;
+  content: '';
+  border: 0.5px solid #fff;
+  opacity: 0.5;
+  width: 50px;
+  margin: 0 0 0 10px;
+  transform: translateY(-4px);
+}
+
+.bottom-img {
+  position: absolute;
+  bottom: 80px;
+  right: 20px;
+  background: url('assets/img/adidas/white-shoe.png') no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 220px;
+  height: 150px;
+}
+
+.bottom-right {
+  position: absolute;
+  bottom: 0;
+  left: 86.5%;
+}
+
+.bottom-right ul {
+  list-style: none;
+}
+
+.bottom-right ul li {
+  display: inline-block;
+  padding: 10px 15px;
+  margin: 20px 20px 20px 0;
+  background: #624652;
+}
+
+.media ul {
+  list-style: none;
+  position: fixed;
+  left: 45px;
+  top: 35%;
+}
+
+.media ul li {
+  font-size: 18px;
+  margin: 24px 0;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0%;
+  z-index: 2;
+  background: #fff;
+}
+
+.first {
+  top: 0;
+}
+
+.second {
+  top: 33.3%;
+}
+
+.third {
+  top: 66.6%;
+}
+</style>
---
+++ pages/black-berry.vue
@@ -0,0 +1,390 @@
+<template>
+      <div class="container">
+
+<nav class="navbar">
+    <div class="logo-icon">
+      <font-awesome-icon :icon="['fas', 'scissors']" />
+    </div>
+    <div class="logo">Black&berry</div>
+    <div class="search">
+      <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+    </div>
+</nav>
+
+<div class="img">
+    <img :src="imgHero" alt="">
+</div>
+
+<div class="title">
+    <h1>
+        <span class="text-one">Black&<label> berry</label></span>
+    </h1>
+    <h2><span>H</span>eart!</h2>
+    <h3>
+        <span class="text-two">cut From <label>the</label></span>
+    </h3>
+    <p>
+        <span class="text-three">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, modi quam. At facilis totam quidem
+            enim nihil voluptatum omnis ea.
+        </span>
+    </p>
+</div>
+
+<div class="line"></div>
+<div class="tag">
+    <span>Because love is in the Hair</span>
+</div>
+
+<button>Visit Shop Now</button>
+
+<div class="bottom left-right icn">
+    <ul>
+        <li>
+          <font-awesome-icon :icon="['fas', 'arrow-left']" />
+        </li>
+        <li>
+          <font-awesome-icon :icon="['fas', 'arrow-right']" />
+        </li>
+    </ul>
+</div>
+
+<div class="media">
+    <ul>
+        <li>facebook</li>
+        <li>instagram</li>
+        <li>snapchat</li>
+    </ul>
+</div>
+</div>
+</template>
+<script lang="js" setup>
+import imgHero from '~/assets/img/black-berry/hero.jpg'
+
+onMounted(() => {
+  TweenMax.from(".logo", 1.6, {
+            delay: .2,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from(".search", 1.6, {
+            delay: .4,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from(".logo-icon", 1.6, {
+            delay: 0,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.staggerFrom(".text-one", 2, {
+            delay: 2.6,
+            opacity: 0,
+            y: 40,
+            ease: Expo.easeInOut
+        }, 0.2)
+
+        TweenMax.staggerFrom(".text-two", 1.5, {
+            delay: 3.4,
+            opacity: 0,
+            y: -40,
+            ease: Expo.easeInOut
+        }, 0.2)
+        TweenMax.staggerFrom(".text-two label", 1, {
+            delay: 4,
+            opacity: 0,
+
+            ease: Expo.easeInOut
+        }, 0.2)
+
+        TweenMax.staggerFrom("h2", 2, {
+            delay: 4,
+            opacity: 0,
+            x: -60,
+            ease: Expo.easeInOut
+        }, 0.2)
+
+        TweenMax.staggerFrom(".text-three", 2, {
+            delay: 4.7,
+            opacity: 0,
+            y: 40,
+            ease: Expo.easeInOut
+        }, 0.2)
+
+        TweenMax.from("button", 2, {
+            delay: 5.5,
+            opacity: 0,
+            y: -30,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from(".line", 2, {
+            delay: 5,
+            opacity: 0,
+            x: -100,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from(".tag", 2, {
+            delay: 5.3,
+            opacity: 0,
+            y: -30,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.staggerFrom(".media ul li", 2, {
+            delay: 6,
+            opacity: 0,
+            y: -40,
+            ease: Expo.easeInOut
+        }, 0.2)
+
+        TweenMax.staggerFrom(".left-right ul li", 2, {
+            delay: 6.4,
+            opacity: 0,
+            y: -40,
+            ease: Expo.easeInOut
+        }, 0.2)
+})
+</script>
+<style lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,800;0,900;1,900&display=swap');
+html,
+body {
+    margin: 0;
+    padding: 0;
+    background-color: #ebecf0;
+    height: 100vh;
+    font-family: 'poppins', sans-serif;
+    overflow: hidden;
+}
+
+.navbar {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    align-items: center;
+}
+
+.navbar>div {
+    padding: 50px 30px;
+    margin: 50px;
+    font-size: 20px;
+}
+
+.navbar .logo-icon {
+    position: relative;
+    left: -20px;
+}
+
+.navbar .logo {
+    position: relative;
+    right: 500px;
+    font-size: 11px;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+
+.img img {
+    width: 47%;
+}
+
+.img {
+    position: fixed;
+    z-index: 0;
+    top: 10.6%;
+    left: 47%;
+}
+
+.title {
+    position: absolute;
+    top: 240px;
+    left: 290px;
+    z-index: 2;
+    height: 100%;
+}
+
+.title h1 {
+    font-size: 60px;
+    text-transform: uppercase;
+    font-weight: 900;
+    letter-spacing: -5px;
+    margin-bottom: 20px;
+    position: relative;
+    height: 150px;
+    width: 600px;
+}
+
+.title h1 .text-one {
+    position: absolute;
+}
+
+.title h1 .text-one label {
+    color: #cccccc;
+}
+
+.title h2 {
+    position: absolute;
+    top: 100px;
+    left: -13px;
+    color: #eda7a7;
+    z-index: -1;
+    font-size: 180px;
+    font-weight: 900;
+    letter-spacing: 2px;
+}
+
+.title h2 span {
+    font-size: 220px;
+}
+
+.title h3 {
+    font-size: 60px;
+    text-transform: uppercase;
+    font-weight: 900;
+    line-height: .8;
+    position: relative;
+    height: 150px;
+    top: -100px;
+}
+
+.title h3 .text-two {
+    position: absolute;
+}
+
+.title h3 .text-two label {
+    position: relative;
+    font-size: 65px;
+    top: 55px;
+    left: -155px;
+}
+
+.title p {
+  width: 500px;
+  font-size: 10px;
+  text-transform: uppercase;
+  font-weight: 500;
+  position: relative;
+  top: 50px;
+  height: 110px;
+}
+
+.tag {
+    position: fixed;
+    top: 166px;
+    left: 70%;
+    padding: 10px 30px;
+    font-weight: 500;
+    color: grey;
+}
+
+.line {
+    position: fixed;
+    left: 71.5%;
+    top: 200px;
+    background-color: #000;
+    width: 230px;
+    height: 1px;
+}
+
+button {
+    font-weight: 900;
+    position: relative;
+    left: 1100px;
+    bottom: -290px;
+    background: #eda7a7;
+    color: #fff;
+    padding: 20px 30px;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-size: 10px;
+    border: none;
+}
+
+.media {
+    position: absolute;
+    left: -120px;
+    top: 50%;
+    transform: rotate(-90deg);
+}
+
+.media ul {
+    list-style: none;
+}
+
+.media ul li {
+    display: inline-block;
+    font-weight: 500;
+    font-size: 15px;
+}
+
+.media ul li:not(:last-child) {
+    padding-right: 60px;
+}
+
+.left-right {
+    position: absolute;
+    right: 40px;
+    bottom: -3%;
+}
+
+.left-right ul {
+    list-style: none;
+}
+
+.left-right ul li {
+    display: inline-block;
+    padding: 10px 15px;
+    margin: 20px 20px 20px 0;
+    font-size: 27px;
+}
+
+img {
+    opacity: 0;
+    animation: img-appear 1s linear forwards;
+    animation-delay: 2.4s;
+}
+
+@keyframes img-appear {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+.img::after {
+    content: "";
+    top: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background: #8c6c79;
+    position: absolute;
+    animation: rev-img 1.5s cubic-bezier(0.19, 1, 0.22, 1) forwards;
+    animation-delay: 2s;
+}
+
+@keyframes rev-img {
+    0% {
+        left: 0%;
+        width: 0%;
+    }
+    50% {
+        left: 0%;
+        width: 47%;
+    }
+    100% {
+        left: 47%;
+        width: 0%;
+    }
+}
+</style>
